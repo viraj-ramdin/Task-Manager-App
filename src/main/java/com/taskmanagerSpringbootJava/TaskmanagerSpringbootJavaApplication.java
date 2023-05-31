7e2bf02e3cb39ec75ebcfe080912dd2d70586a97
Viraj Ramdin <viraj.ramdin@accolite.com>
2023-05-31 11:51:02 +0530

    #1 Project setup via spring initializer

---
+++ src/main/java/com/taskmanagerSpringbootJava/TaskmanagerSpringbootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanagerSpringbootJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerSpringbootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerSpringbootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskmanagerSpringbootJava/TaskmanagerSpringbootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanagerSpringbootJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerSpringbootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
